3069ec0995aba82408c21e54b6ab9a64086e6340
srikanthsrnvs <srikanth.srinivas@mail.utoronto.ca>
2023-02-23 20:49:23 -0800

    Push fixes

--- chat.py
+++ chat.py
@@ -1,6 +1,7 @@
 from time import time
 from typing import List
 from uuid import uuid4
+import os
 
 import openai
 
@@ -8,6 +9,7 @@ from constants import USERNAME, BOT_NAME
 from gpt3_helpers import vector_similarity, gpt3_embedding, gpt3_completion
 from models import Message, Conversation, Note
 from utils import open_file, save_json, timestamp_to_datetime
+from llama_index import GPTTreeIndex, Document
 
 
 def fetch_memories(vector, logs, count):
@@ -41,12 +43,13 @@ def summarize_memories(memories):  # summarize a block of memories into one payl
     prompt = open_file('prompt_notes.txt').replace('<<INPUT>>', block)
     # TODO - do this in the background over time to handle huge amounts of memories
     notes = gpt3_completion(prompt)
-    notes.split('\n')
-    vector = gpt3_embedding(block)
-    info = {'notes': notes, 'uuids': identifiers, 'times': timestamps, 'uuid': str(uuid4()), 'vector': vector}
-    filename = 'notes_%s.json' % time()
-    save_json('notes/%s' % filename, info)
     return notes
+    # notes.split('\n')
+    # vector = gpt3_embedding(block)
+    # info = {'notes': notes, 'uuids': identifiers, 'times': timestamps, 'uuid': str(uuid4()), 'vector': vector}
+    # filename = 'notes_%s.json' % time()
+    # save_json('notes/%s' % filename, info)
+    # return notes
 
 
 def get_last_messages(conversation, limit):
@@ -90,68 +93,98 @@ def summarize_notes(notes):
     result = gpt3_completion(prompt)
     return [Note(i.strip()) for i in result.split('- ')]
 
+def open_index_file():
+    file_name = 'index.json'
+    try:
+        # Open the file for reading
+        with open(file_name, 'r') as file:
+            # Read the contents of the file
+            index = GPTTreeIndex.load_from_disk(file_name)
+            print('Loaded index from: ', file_name)
+            return index
+    except FileNotFoundError:
+        # If the file does not exist, create it and write some default text
+        with open(file_name, 'w') as file:
+            tree_index = GPTTreeIndex([])
+            print('No index file found. Created: ', file_name)
+            return tree_index
 
 def main():
     conversation = Conversation.load()
+    index = open_index_file()
     while True:
 
-        # step 1 - get input
-        # step 2 - gather all information about the conversation (load memories, notes, wikipedia maybe?)
-        # step 3 - generate search queries to search info about the input
-        # step 4 - use vector search to find information in our conversation/other information sources
-        # step 5 - compile an answer using such information
-        # step 6 - put out an answer
-        # step 7 - create a memory of the conversation
-        # step 8 - repeat
+        # Step 1: Get the current context window, ie last 12 messages.
+        # Step 2: Get the current message, search all indexed memories to retrieve facts that have been said
+        # Step 3: Feed Current context window, and search results to a language model to predict next response
+        # Step 4: Add message to conversation. If conversation.message size is a multiple of 12, create a new document from the last 12 messages, and summarize it and index it
+        # Step 5: Store this new memory by adding it to the index
 
         # step 1
         message = get_user_input()
-        conversation.add_message(message)
+        context_window = conversation.get_last_messages_in_string(12)
 
         # step 2
+        index_search_prompt = (
+            open_file('prompts/prompt_index_search.txt')
+            .replace('<<CURRENT QUERY>>', message.get_string())
+        )
         last_6_messages = conversation.get_last_messages_in_string(12)  # get last 6 messages as a string
-        notes = conversation.get_notes_as_string()  # get notes from the conversation
-        # gather_info_prompt = (
-        #     open_file('prompts/prompt_conversation_prepare_info.txt')
-        #     .replace('<<CONVERSATION>>', last_6_messages)
-        #     .replace('<<NOTES>>', notes)
-        # )
-        # step 3
-        # search_queries = [i.strip() for i in gpt3_completion(gather_info_prompt).split('- ') if i.strip() != '']
-        # step 4
-        related_messages = search_conversation(conversation, message)
-        # facts = [f"Question: {i}; Answer: {input(i)}" for i in search_queries] # lmao
-        # step 5
-        if len(related_messages) > 10:
-            answer_prompt = (
-                open_file('prompts/prompt_response.txt')
-                .replace('<<CONVERSATION>>', last_6_messages)
-                .replace('<<NOTES>>', notes)
-                .replace('<<MESSAGES_RELATED>>', '\n'.join([i.get_string() for i in related_messages]))
-                # .replace('<<FACTS>>', '\n'.join(facts))
-            )
-        else:
-            answer_prompt = (
-                open_file('prompts/prompt_response_in_new_conversation.txt').replace('<<CONVERSATION>>', last_6_messages)
-            )
-        # step 6
-        answer = gpt3_completion(answer_prompt)
-        print(f'{BOT_NAME}: {answer}')
-        conversation.add_message(Message(BOT_NAME, answer))
-        # step 7
-        notes_prompt = open_file('prompts/prompt_notes.txt').replace('<<INPUT>>', last_6_messages)
-        notes = [i.strip() for i in gpt3_completion(notes_prompt).split('- ')]
+        search_results = index.query(index_search_prompt)
 
-        [conversation.add_note(Note(note)) for note in notes]
-        if len(conversation.get_notes()) > 10:
-            # compress notes
-            notes = conversation.get_notes()
-            notes = summarize_notes(notes)
-            conversation.set_notes(notes)
-        # step 8
+        # step 3
+        response_prompt = (
+            open_file('prompts/prompt_response.txt')
+            .replace('<<CONVERSATION>>', last_6_messages)
+            .replace('<<MEMORIES>>', search_results)
+            .replace('<<CHAT>>', message.get_string())
+        )
+        response = gpt3_completion(response_prompt)
+        print("Liza: %s" % response)
+
+        # step 4
+        
+        if len(conversation.get_messages()) % 12 == 0:
+            # create a new memory
+            new_memory = summarize_memories(conversation.get_last_messages(12))
+            index.insert(Document(new_memory))
+        conversation.add_message(message)
+        # # step 3
+        # search_queries = [i.strip() for i in gpt3_completion(gather_info_prompt).split('- ') if i.strip() != '']
+        # # step 4
+        # related_messages = search_conversation(conversation, message)
+        # # facts = [f"Question: {i}; Answer: {input(i)}" for i in search_queries] # lmao
+        # # step 5
+        # if len(related_messages) > 10:
+        #     answer_prompt = (
+        #         open_file('prompts/prompt_response.txt')
+        #         .replace('<<CONVERSATION>>', last_6_messages)
+        #         .replace('<<NOTES>>', notes)
+        #         .replace('<<MESSAGES_RELATED>>', '\n'.join([i.get_string() for i in related_messages]))
+        #         # .replace('<<FACTS>>', '\n'.join(facts))
+        #     )
+        # else:
+        #     answer_prompt = (
+        #         open_file('prompts/prompt_response_in_new_conversation.txt').replace('<<CONVERSATION>>', last_6_messages)
+        #     )
+        # # step 6
+        # answer = gpt3_completion(answer_prompt)
+        # print(f'{BOT_NAME}: {answer}')
+        # conversation.add_message(Message(BOT_NAME, answer))
+        # # step 7
+        # notes_prompt = open_file('prompts/prompt_notes.txt').replace('<<INPUT>>', last_6_messages)
+        # notes = [i.strip() for i in gpt3_completion(notes_prompt).split('- ')]
+
+        # [conversation.add_note(Note(note)) for note in notes]
+        # if len(conversation.get_notes()) > 10:
+        #     # compress notes
+        #     notes = conversation.get_notes()
+        #     notes = summarize_notes(notes)
+        #     conversation.set_notes(notes)
+        # # step 8
         conversation.save()
 
 
 if __name__ == '__main__':
-    openai.api_key = open_file('openaiapikey.txt')
+    openai.api_key = os.getenv('OPENAI_API_KEY')
     main()
